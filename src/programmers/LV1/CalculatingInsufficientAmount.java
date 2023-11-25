72bde9777e7387b70aeed05bbd585c7d0bca5e5f
grace <grace994084@gmail.com>
2023-11-25 23:21:40 +0900

    feat : 부족한 금액 계산하기_LV1

---
+++ src/programmers/LV1/CalculatingInsufficientAmount.java
@@ -0,0 +1,19 @@
+package programmers.LV1;
+
+public class CalculatingInsufficientAmount {
+
+	public static void main(String[] args) {
+		int price = 3;
+		int money = 20;
+		int count = 4;
+		System.out.println(solution(price, money, count));
+	}
+
+	public static long solution(int price, int money, int count) {
+		long sum = 0;
+		for (int i = 1; i <= count; i++) {
+			sum += price * i;
+		}
+		return money >= sum ? 0 : sum - money;
+	}
+}
